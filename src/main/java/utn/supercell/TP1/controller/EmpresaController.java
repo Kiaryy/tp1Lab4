c63e3c8d4d79160e2a0455175d946cc311ba0d44
Kiaryy <martin360.moron@gmail.com>
2025-03-26 16:40:19 -0300

    Updated cors policies

--- src/main/java/utn/supercell/TP1/controller/EmpresaController.java
+++ src/main/java/utn/supercell/TP1/controller/EmpresaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import utn.supercell.TP1.service.EmpresaService;
 
 @RestController
 @RequestMapping("/empresa/")
+@CrossOrigin(origins = "*")
 public class EmpresaController {
     @Autowired
     private EmpresaService empresaService;
